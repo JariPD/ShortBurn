73525ba2a0a27f37fccc68f4a750bd3c55c5f599
Jari Dijk <dijkjari@gmail.com>
2022-06-23 11:24:05 +0200

    brick puzzle in scene

--- ShortBurn/Assets/Scripts/Puzzles/MoveObjectPuzzle.cs
+++ ShortBurn/Assets/Scripts/Puzzles/MoveObjectPuzzle.cs
@@ -7,18 +7,14 @@ public class MoveObjectPuzzle : MonoBehaviour
     [SerializeField] private Quaternion targetRot;
     [SerializeField] private float rotSpeed;
     [SerializeField] private float moveSpeed;
-    private Vector3 resetPos;
 
     public bool MoveObject;
-    public bool ResetDoor;
 
     private AudioSource audioSource;
 
     private void Start()
     {
         audioSource = GetComponent<AudioSource>();
-
-        resetPos = transform.localPosition;
     }
 
     void Update()
@@ -26,9 +22,6 @@ public class MoveObjectPuzzle : MonoBehaviour
         if (gameObject.CompareTag("Brick") && MoveObject)
         {
             AudioManager.instance.Play("Brick Puzzle");
-
-            //if (Vector3.Distance(transform.position, targetPos) <= 0.01f)
-            //bool a = Mathf.Approximately(transform.position.z, targetPos.z);
 
             if (transform.localPosition == targetPos)
                 AudioManager.instance.Stop("Brick Puzzle");
@@ -38,10 +31,6 @@ public class MoveObjectPuzzle : MonoBehaviour
         {
             transform.localRotation = Quaternion.RotateTowards(transform.localRotation, targetRot, rotSpeed * Time.deltaTime);
             transform.localPosition = Vector3.MoveTowards(transform.localPosition, targetPos, moveSpeed * Time.deltaTime);
-        }
-        else if (ResetDoor)
-        {
-            transform.localPosition = Vector3.MoveTowards(transform.localPosition, resetPos, moveSpeed * Time.deltaTime);
         }
     }
 }
